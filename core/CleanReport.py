fc90cbbfc06459064b2ce5cd4f5a401693c7db33
Matthias Friedmann <matthias.friedmann@me.com>
2018-05-15 08:59:17 +0200

    Resetting project

--- core/CleanReport.py
+++
@@ -1,9 +0,0 @@
-""" 
-                    report_text = full_text.get_text()
-                    report_text = report_text.replace("/", " / ")
-
-                    # Workaround: BS lässt keinen Whitespace am Ende von Absätzen.
-                    # Beginnt dann ein Absatz mit einem Namen, wird dieser nicht erkannt.
-                    report_text = re.sub(r"\.([a-zA-Z])", ". \\1", report_text)
-
-"""
--- core/SentimentExtractor.py
+++
@@ -1,1 +0,0 @@
-SentimentExtractor.py
